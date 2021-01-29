1da796d08a42a89f46a1667b7d7c18e5a63aa72c
Grzegorz <grzesiu1997@gmail.com>
2021-01-29 22:54:48 +0100

    make 7 outputs file of 6 minor fixes

---
+++ Scripts/7.ps1
@@ -0,0 +1,12 @@
+﻿$fileName = "zablokowane konta"
+
+Write-Host "Generowanie raportu - lista zablokowanych kont w domenie"
+Write-Host ""
+
+New-Item -Path $workDir\Outputs -Name "$fileName.txt" -Force
+$users = Search-ADAccount -LockedOut | Sort-Object name | Select-Object name
+foreach ($user in $users)
+{
+    $onlyUserName = $user.name
+    Add-Content -Path "$workDir\Outputs\$onlyGroupName.txt" -Value $onlyUserName
+}
