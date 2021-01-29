1aecc7dfae23151d16ea48af68c8ee71c0ef7571
shakra125223 <78226719+shakra125223@users.noreply.github.com>
2021-01-29 22:02:31 +0100

    Update 3.ps1

--- Scripts/3.ps1
+++ Scripts/3.ps1
@@ -1,37 +1,15 @@
 ﻿#Generacja pustego plik
-write-host "Podaj nazwe użytkownika do zablokowania"
-$path="$workdir\inputs\importusersblank.csv"
-$header ="login|hasło|dział"
-$selection = Read-Host "Proszę dokonać wyboru:"
-
-      
+$path="$workdir\outputs\zablokowane konta data.txt"
+$header ="Zablokowany użytkownik|data|Zablokowany przez użytkownika"
+blockuser    
 function blockuser
 {
-    Write-host "Plik powinne się znajdować w folderze inputs!"
-    $read = read-host -Prompt "Podaj nazwe pliku"
-    $users = Import-Csv $workdir/inputs/$file.csv -Delimiter "|" -Encoding Default
-    foreach ($user in $users)
-    {
-        $login = $user.login
-        $password = $user.haslo
-        $SecurityPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-        $department = $user.dzial
-        $firstname, $lastname = $login.split(".")[0, -1]
-        $domain = (Get-ADDomain).dnsroot
-        $displayname = $firstname + ” ” + $lastname
-        $mail = $login + „@” + $domain
-        $inc = 0
-        $sam = $login
-        if (Get-ADuser -Filter { SamAccountName -eq $login })
-        {
-            do
-            {
-                $inc++
-                $login = $sam + $inc
-            }
-            until(-not(Get-ADuser -Filter { SamAccountName -eq $login }))
+        $login = read-host "Podaj nazwe użytkownika do zablokowania"
+        try {
+           Get-ADuser -SamAccountName $login | out-null
+           Disable-ADAccount -name $login
         }
-        New-ADUser -Name $login -DisplayName $displayname -SamAccountName $login -UserPrincipalName "$mail" -GivenName "$firstname" -Surname "$lastname" -Department $Department -AccountPassword $securityPassword -Enabled $true -Path "DC=$( $domain.Split(".")[0] ),DC=$( $domain.Split(".")[1] )" -ChangePasswordAtLogon $true -PasswordNeverExpires $false
-
-    }
+        catch  {
+            write-host = "Nie poprawna nazwa użytkownika"
+        }
 }
