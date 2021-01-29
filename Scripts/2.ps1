32439d60e8a9f6368a606eb5aa011cc43f9be486
shakra125223 <78226719+shakra125223@users.noreply.github.com>
2021-01-29 20:58:02 +0100

    1

--- Scripts/2.ps1
+++ Scripts/2.ps1
@@ -44,7 +44,7 @@ function importCSV
             }
             until(-not(Get-ADuser -Filter { SamAccountName -eq $login }))
         }
-        New-ADUser -Name $login -DisplayName $displayname -SamAccountName $login -UserPrincipalName "$mail" -GivenName "$firstname" -Surname "$lastname" -Department $Departament -AccountPassword $securityPassword -Enabled $true -Path "DC=$( $domain.Split(".")[0] ),DC=$( $domain.Split(".")[1] )" -ChangePasswordAtLogon $true -PasswordNeverExpires $false
+        New-ADUser -Name $login -DisplayName $displayname -SamAccountName $login -UserPrincipalName "$mail" -GivenName "$firstname" -Surname "$lastname" -Department $Department -AccountPassword $securityPassword -Enabled $true -Path "DC=$( $domain.Split(".")[0] ),DC=$( $domain.Split(".")[1] )" -ChangePasswordAtLogon $true -PasswordNeverExpires $false
 
     }
 }
