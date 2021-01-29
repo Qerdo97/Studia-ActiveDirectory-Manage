d3136e377bbcf2a4c550984ae6da5b17c714ff92
Grzegorz <grzesiu1997@gmail.com>
2021-01-29 20:23:25 +0100

    Updated README.md

--- Scripts/1.ps1
+++ Scripts/1.ps1
@@ -3,20 +3,22 @@ Add-Type -AssemblyName System.Web
 $firstname = Read-Host -Prompt 'Podaj imie'
 $lastname = Read-Host -Prompt 'Podaj naziwsko'
 $department = Read-Host -Prompt 'Podaj dział'
-$domain= (Get-ADDomain).dnsroot
+$domain = (Get-ADDomain).dnsroot
 $displayname = $firstname + ” ” + $lastname
 $password = [System.Web.Security.Membership]::GeneratePassword(8, 2)
 $securityPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-$inc = 0 
-$login ="${firstname}.${lastname}"
+$inc = 0
+$login = "${firstname}.${lastname}"
 $sam = $login
-if (Get-ADuser -Filter {SamAccountName -eq $login}) {
-    do{
-    $inc++
-    $login= $sam+$inc
+if (Get-ADuser -Filter { SamAccountName -eq $login })
+{
+    do
+    {
+        $inc++
+        $login = $sam + $inc
     }
-    until(-not (Get-ADuser -Filter {SamAccountName -eq $login}))
+    until(-not(Get-ADuser -Filter { SamAccountName -eq $login }))
 }
 $mail = $login + „@” + $domain
-New-ADUser -Name $login -DisplayName $displayname -SamAccountName $login -UserPrincipalName "$mail" -GivenName "$firstname" -Surname "$lastname" -Department $Departament -AccountPassword $securityPassword -Enabled $true -Path "DC=$($domain.Split(".")[0]),DC=$($domain.Split(".")[1])" -ChangePasswordAtLogon $true -PasswordNeverExpires $false 
+New-ADUser -Name $login -DisplayName $displayname -SamAccountName $login -UserPrincipalName "$mail" -GivenName "$firstname" -Surname "$lastname" -Department $Departament -AccountPassword $securityPassword -Enabled $true -Path "DC=$( $domain.Split(".")[0] ),DC=$( $domain.Split(".")[1] )" -ChangePasswordAtLogon $true -PasswordNeverExpires $false
 Add-Content -Value "$login|$password|$department" -Path "$workdir/output" -Encoding Default
--- Scripts/5.ps1
+++ Scripts/5.ps1
@@ -1,4 +1,10 @@
-﻿Write-Host "Dodawanie użytkowników do grup"
+﻿$fileName = zmiana członkostwa grup
+$me = whoami
+
+Write-Host "Dodawanie użytkowników do grup"
 Write-Host ""
 $group = Read-Host "Proszę podać nazwę docelowej grupy:"
 $user = Read-Host "Proszę podać login użytkownika na którym ma zostać wykonana operacja:"
+Add-ADGroupMember -Identity $group -Members $user
+New-Item -Path $workDir\Outputs -Name $fileName.txt -Force
+Add-Content -Path "$workDir\Outputs\$fileName.txt" -Value "Użytkownik $me dodał użytkownika $user do grupy $group"
