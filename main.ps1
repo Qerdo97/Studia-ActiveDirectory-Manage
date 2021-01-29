256095891f4ed1a79bd8965f32a583fb8db43ae5
Grzegorz <grzesiu1997@gmail.com>
2021-01-29 19:17:11 +0100

    Menu

--- main.ps1
+++ main.ps1
@@ -1,66 +1,84 @@
-$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
-$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+﻿$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8BOM'
+$PSDefaultParameterValues['*:Encoding'] = 'utf8BOM'
 
 function Authors
 {
 Write-Host    "AZ1 Poprawa"
+Write-Host    ""
 Write-Host    "Authors:"
 Write-Host    "Grzegorz Sekuła"
+Write-Host    "Nr indeksu: 17313"
+Write-Host    ""
 Write-Host    "Robert Łobasiuk"
+Write-Host    "Nr indeksu: 17266"
 }
 
 function Show-Menu
 {
-    param (
-        [string]$Title = 'Menu'
-    )
     Clear-Host
-    Write-Host "================ $Title ================"
-
-    Write-Host "1: Press '1' for this option."
-    Write-Host "2: Press '2' for this option."
-    Write-Host "3: Press '3' for this option."
-    Write-Host "4: Press '4' for this option."
-    Write-Host "5: Press '5' for this option."
-    Write-Host "6: Press '6' for this option."
-    Write-Host "7: Press '7' for this option."
-    Write-Host "8: Press '8' for this option."
-    Write-Host "A: Press 'A' for info about authors."
-    Write-Host "Q: Press 'Q' to quit."
+    Write-Host "================ MENU ================"
+    Write-Host ""
+    Write-Host "    == Obsługa kont użytkowników ==   "
+    Write-Host "1: Wciśnij '1' aby stworzyć konto użytkownika."
+    Write-Host "2: Wciśnij '2' aby stworzyć wiele kont na podstawie pliku csv."
+    Write-Host "3: Wciśnij '3' aby zablokować konto użytkownika."
+    Write-Host "4: Wciśnij '4' aby zmienić hasło do konta użytkownika."
+    Write-Host ""
+    Write-Host "        == Obsługa kont grup ==       "
+    Write-Host "5: Wciśnij '5' aby dodać użytkowników do grup."
+    Write-Host ""
+    Write-Host "             == Raporty ==            "
+    Write-Host "6: Wciśnij '6' aby otrzymać listę grup z członkami."
+    Write-Host "7: Wciśnij '7' aby otrzymać listę zablokowanych kont w domenie."
+    Write-Host ""
+    Write-Host "             == Więcej ==             "
+    Write-Host "A: Wciśnij 'A' aby otrzymać informacje o autorach."
+    Write-Host "Q: Wciśnij 'Q' aby wyjść."
+    Write-Host ""
+    Write-Host "======================================"
 }
 
 #Wyświetlenie menu i oczekiwanie na decyzję
 do
 {
     Show-Menu
-    $selection = Read-Host "Please make a selection"
+    $selection = Read-Host "Proszę dokonać wyboru:"
     switch ($selection)
     {
         '1' {
+            cls
             'You chose option #1'
         }
         '2' {
+            cls
             'You chose option #2'
         }
         '3' {
+            cls
             'You chose option #3'
         }
         '4' {
+            cls
             'You chose option #4'
         }
         '5' {
+            cls
             'You chose option #5'
         }
         '6' {
+            cls
             'You chose option #6'
         }
         '7' {
+            cls
             'You chose option #7'
         }
         '8' {
+            cls
             'You chose option #8'
         }
         'a' {
+            cls
             Authors
         }
     }
