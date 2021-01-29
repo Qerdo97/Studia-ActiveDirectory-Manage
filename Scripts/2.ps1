157abf233401b4107fe8b22cf1f082fb89b7c196
shakra125223 <78226719+shakra125223@users.noreply.github.com>
2021-01-30 00:31:41 +0100

    Update 2.ps1

--- Scripts/2.ps1
+++ Scripts/2.ps1
@@ -1,16 +1,16 @@
 ﻿#Generacja pustego plik
 write-host "1. Generacja pustego pliku CSV z nagłówkami"
 write-host "2. Tworzenie użytkowników z pliku CSV"
-$path="$workdir\inputs\uzytkownicy.csv"
+$path="$workdir\inputs\uzytkownicyblank.csv"
 $header ="login|hasło|dział"
 function generateCSV
 {
-remove-item $path
+if (-not( Test-path $path)){
 New-Item $path |Add-Content -Value $header -Encoding Default
+}
 }
 function importCSV
 {
-    Write-host "Plik powinne się znajdować w folderze inputs!"
     $users = Import-Csv $workdir/inputs/uzytkownicy.csv -Delimiter "|" -Encoding Default
     foreach ($user in $users)
     {
@@ -21,7 +21,6 @@ function importCSV
         $firstname, $lastname = $login.split(".")[0, -1]
         $domain = (Get-ADDomain).dnsroot
         $displayname = $firstname + ” ” + $lastname
-        $mail = $login + „@” + $domain
         $inc = 0
         $sam = $login
         if (Get-ADuser -Filter { SamAccountName -eq $login })
@@ -33,18 +32,17 @@ function importCSV
             }
             until(-not(Get-ADuser -Filter { SamAccountName -eq $login }))
         }
+        $mail = $login + „@” + $domain
         New-ADUser -Name $login -DisplayName $displayname -SamAccountName $login -UserPrincipalName "$mail" -EmailAddress $mail -GivenName "$firstname" -Surname "$lastname" -Department $Department -AccountPassword $securityPassword -Enabled $true -Path "DC=$( $domain.Split(".")[0] ),DC=$( $domain.Split(".")[1] )" -ChangePasswordAtLogon $true -PasswordNeverExpires $false
-    }
+        }
 }
 $choose = Read-Host "Proszę dokonać wyboru:"
 switch ($choose)
 {
     '1' {
-        Clear-Host
         generateCSV
     }
     '2' {
-        Clear-Host
         importCSV
     }
 }
