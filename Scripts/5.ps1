685554806670232eb1af9887f6085af3a786f0eb
Grzegorz <grzesiu1997@gmail.com>
2021-01-29 21:23:49 +0100

    5 updated for try and catch

--- Scripts/5.ps1
+++ Scripts/5.ps1
@@ -1,10 +1,17 @@
-﻿$fileName = zmiana członkostwa grup
+﻿$fileName = "zmiana członkostwa grup"
 $me = whoami
 
 Write-Host "Dodawanie użytkowników do grup"
 Write-Host ""
 $group = Read-Host "Proszę podać nazwę docelowej grupy"
 $user = Read-Host "Proszę podać login użytkownika na którym ma zostać wykonana operacja"
-Add-ADGroupMember -Identity $group -Members $user
-New-Item -Path $workDir\Outputs -Name $fileName.txt -Force
-Add-Content -Path "$workDir\Outputs\$fileName.txt" -Value "Użytkownik $me dodał użytkownika $user do grupy $group"
+try {
+    Add-ADGroupMember -Identity $group -Members $user
+    New-Item -Path $workDir\Outputs -Name $fileName.txt -Force
+    Add-Content -Path "$workDir\Outputs\$fileName.txt" -Value "Użytkownik $me dodał użytkownika $user do grupy $group"
+}
+catch {
+    Write-Host "Nie udało się wykonać operacji"
+    Write-Host "Brak takiej grupy lub użytkownika"
+    Write-Host "Wystąpił błąd: $Error"
+}
