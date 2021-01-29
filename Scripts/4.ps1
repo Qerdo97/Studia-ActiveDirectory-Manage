e18e04d1632f0fae8763bfdb892a4865a2b998c3
Grzegorz <grzesiu1997@gmail.com>
2021-01-29 23:21:35 +0100

    Clean code

--- Scripts/4.ps1
+++ Scripts/4.ps1
@@ -1,31 +1,35 @@
 ﻿#Generacja pustego plik
-$path="$workdir\outputs\zmienione hasła.txt"
-$header ="Zmieniono hasło dla użytkownika|Data|Zmieniono przez użytkownika"
-if (-not( Test-path $path)){
-New-Item $path |Add-Content -Value $header -Encoding Default
+$path = "$workdir\outputs\zmienione hasła.txt"
+$header = "Zmieniono hasło dla użytkownika|Data|Zmieniono przez użytkownika"
+if (-not( Test-path $path))
+{
+    New-Item $path |Add-Content -Value $header -Encoding Default
 }
 function changepassword
 {
     $domain = (Get-ADDomain).name
     $login = read-host "Podaj nazwe użytkownika do zmiany hasła"
     $password = read-host "Podaj hasło"
-    try {
-    $login = Get-ADuser -Filter { SamAccountName -eq $login }
-    Set-ADAccountPassword -identity $login -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force)
-    Set-ADUser -identity $login -SmartcardLogonRequired $True
-    $time = Get-Date
-    $user= whoami
-    $blockeduser=$login.SamAccountName
-    Add-Content -value "$domain\$blockeduser|$time|$user" -path $path
-    write-host "Hasło zostało zmienione"
+    try
+    {
+        $login = Get-ADuser -Filter { SamAccountName -eq $login }
+        Set-ADAccountPassword -identity $login -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force)
+        Set-ADUser -identity $login -SmartcardLogonRequired $True
+        $time = Get-Date
+        $user = whoami
+        $blockeduser = $login.SamAccountName
+        Add-Content -value "$domain\$blockeduser|$time|$user" -path $path
+        write-host "Hasło zostało zmienione"
     }
-    catch [Microsoft.ActiveDirectory.Management.ADPasswordComplexityException]{
-    Write-Output $_.Exception
+    catch [Microsoft.ActiveDirectory.Management.ADPasswordComplexityException]
+    {
+        Write-Output $_.Exception
         write-host "Hasło jest zbyt słabe"
 
     }
-    catch{
-    write-host "Użytkownik nie istnieje"
+    catch
+    {
+        write-host "Użytkownik nie istnieje"
     }
 
 
