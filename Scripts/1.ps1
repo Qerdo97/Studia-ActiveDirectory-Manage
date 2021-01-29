066135568b139a03f30d45dc47b85cf574fcd6a0
shakra125223 <78226719+shakra125223@users.noreply.github.com>
2021-01-29 23:28:34 +0100

    Update 1.ps1

--- Scripts/1.ps1
+++ Scripts/1.ps1
@@ -10,7 +10,7 @@ $lastname = Read-Host -Prompt 'Podaj naziwsko'
 $department = Read-Host -Prompt 'Podaj dział'
 $domain = (Get-ADDomain).dnsroot
 $header = "Login|Hasło"
-$path="$workdir\inputs\nazwa użytkownika.txt"
+$path="$workdiroutputs/nazwa użytkownika.txt"
 $displayname = $firstname + ” ” + $lastname
 $password = Get-RandomCharacters -length 7 -characters 'abcdefghiklmnoprstuvwxyz'
 $password += Get-RandomCharacters -length 2 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
@@ -35,5 +35,4 @@ if (Get-ADuser -Filter { SamAccountName -eq $login })
 }
 $mail = $login + „@” + $domain
 New-ADUser -Name $login -DisplayName $displayname -SamAccountName $login -UserPrincipalName "$mail" -GivenName "$firstname" -Surname "$lastname" -Department $department -AccountPassword $securityPassword -Enabled $true -Path "DC=$( $domain.Split(".")[0] ),DC=$( $domain.Split(".")[1] )" -ChangePasswordAtLogon $true -PasswordNeverExpires $false
-New-Item $path |Add-Content -Value $header -Encoding Default
-Add-Content -Value "$login|$password" -Path "$workdir/outputs/nazwa użytkownika.txt" -Encoding Default
+Add-Content -Value "$login|$password" -Path $path -Encoding Default
